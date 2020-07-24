137492b2b9fcc45a97fd5448799c69371f42dd0d
DESKTOP-O3BQ6OP\arthu <arthurrib@gmail.com>
2020-07-24 03:20:07 -0300

    adicionados plugins de slider e page builder

---
+++ wp-content/themes/understrap/footer.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+
+<div class="wrapper" id="wrapper-footer">
+
+	<div class="<?php echo esc_attr( $container ); ?>">
+
+		<div class="row">
+
+			<div class="col-md-4">
+				<div class="card">
+					<h1>Card 1</h1>
+				</div>
+			</div><!--col end -->
+
+			<div class="col-md-4">
+			<div class="card">
+					<h1>Card 2</h1>
+				</div>
+			</div><!--col end -->
+
+			<div class="col-md-4">
+			<div class="card">
+					<h1>Card 3</h1>
+				</div>
+			</div><!--col end -->
+
+		</div><!-- row end -->
+
+	</div><!-- container end -->
+
+</div><!-- wrapper end -->
+
+</div><!-- #page we need this extra closing tag here -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
+
